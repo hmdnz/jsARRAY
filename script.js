a6c6935927172eecd7a2162a348a79774609457f
hmdnz <hammadmcferri@gmail.com>
2024-04-15 18:28:53 +0100

    1st commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hello")
+let names=["Ahmadu", "Bello", "Yakubu", "Gowon"]
+let names2=["Murtala", "Muhammed", "Olusegun", "Obasanjo"]
+let names3=["Shehu", "Shagari","Muhammadu", "Buhari"]
+// concat - Joins diffrent variables into a new variable
+
+let joined=names.concat(names2, names3)
+console.log(joined)
+
+//Splice use to remove or add elements to array
+
+joined.splice()
+console.log(joined)
